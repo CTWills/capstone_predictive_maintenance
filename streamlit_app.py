e937a95ea0f249c6e1443ce0742e915f8704b233
willsc <cameron.wills.1998@gmail.com>
2024-08-06 08:45:18 -0600

    created streamlits requirements txt file

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,31 +29,24 @@ if __name__ == '__main__':
         "M": [0]
     })
 
-    with st.sidebar:
-        classifier = st.checkbox(
-            "Failure Classifier",
-        )
+    st.title("Failure Classification")
+    col1, col2 = st.columns(2, gap="large")
 
-   
-    if classifier:
-        st.title("Failure Classification")
-        col1, col2 = st.columns(2, gap="large")
+    with col1:
+        air_temp = st.number_input("Air temperature [K]", value=295.2)
+        sensor_data["Air temperature [K]"] = [air_temp]
 
-        with col1:
-            air_temp = st.number_input("Air temperature [K]", value=295.2)
-            sensor_data["Air temperature [K]"] = [air_temp]
+        proc_temp = st.number_input("Process temperature [K]", value=308.4)
+        sensor_data["Process temperature [K]"] = [proc_temp]
 
-            proc_temp = st.number_input("Process temperature [K]", value=308.4)
-            sensor_data["Process temperature [K]"] = [proc_temp]
+        rot_speed = st.number_input("Rotational speed [rpm]", value=1310)
+        sensor_data["Rotational speed [rpm]"] = [rot_speed]
 
-            rot_speed = st.number_input("Rotational speed [rpm]", value=1310)
-            sensor_data["Rotational speed [rpm]"] = [rot_speed]
+        torque = st.number_input("Torque [Nm]", value=61.0)
+        sensor_data["Torque [Nm]"] = [torque]
 
-            torque = st.number_input("Torque [Nm]", value=61.0)
-            sensor_data["Torque [Nm]"] = [torque]
+        tool_wear = st.number_input("Tool wear [min]", value=189)
+        sensor_data["Tool wear [min]"] = [tool_wear]
 
-            tool_wear = st.number_input("Tool wear [min]", value=189)
-            sensor_data["Tool wear [min]"] = [tool_wear]
-
-        with col2:
-            display_classification(models, sensor_data)
+    with col2:
+        display_classification(models, sensor_data)
